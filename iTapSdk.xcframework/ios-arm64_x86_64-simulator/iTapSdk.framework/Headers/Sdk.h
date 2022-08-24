59826655f80de3d934c3d857e21bd8de3bf7e100
Minhvu6533 <minhvv@vtvlive.vn>
2022-08-24 17:22:24 +0700

    no message

---
+++ iTapSdk.xcframework/ios-arm64_x86_64-simulator/iTapSdk.framework/Headers/Sdk.h
@@ -0,0 +1,16 @@
+//
+//  Sdk.h
+//  SdkFramework
+//
+//  Created by Minh Vu on 24/08/2022.
+//
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Sdk : NSObject
+- (void) printSdk;
+
+@end
+
+NS_ASSUME_NONNULL_END
